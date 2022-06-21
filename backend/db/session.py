af711ed1e2e86c8ecf26a24aad5dc09efaf88df7
pbs23th <pbs23th1111Wgmail.com>
2022-06-21 17:01:10 +0900

    password hasher

--- backend/db/session.py
+++ backend/db/session.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from backend.core.config import settings
-
+from typing import Generator
 
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
@@ -16,3 +16,11 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 # )
 
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+
+def get_db() -> Generator:   #new
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
