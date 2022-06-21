a314ea00495988e1326c209a8cae4fd12e75a1cb
pbs23th <pbs23th1111Wgmail.com>
2022-06-21 16:54:46 +0900

    create many

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,7 +1,19 @@
-#config.py
+import os
+from dotenv import load_dotenv
+
+from pathlib import Path
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
 class Settings:
     PROJECT_NAME:str = "Job Board"
     PROJECT_VERSION: str = "1.0.0"
+
+    MYSQL_USER: str = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_SERVER: str = os.getenv("MYSQL_SERVER","localhost")
+    MYSQL_PORT: str = os.getenv("MYSQL_PORT",3306) # default MYSQL port is 5432
+    MYSQL_DB: str = os.getenv("MYSQL_DB","test")
+    DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_SERVER}:{MYSQL_PORT}/{MYSQL_DB}"
 
 settings = Settings()
---
+++ backend/db/base.py
@@ -0,0 +1,4 @@
+#db > base.py
+from backend.db.base_class import Base
+from backend.db.models.jobs import Job
+from backend.db.models.users import User
