7313336e1f0ea11d639b251221aa666405e4fc4c
pbs23th <pbs23th1111Wgmail.com>
2022-06-21 16:20:02 +0900

    create templates

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,26 @@
 #main.py
-
+import uvicorn
 from fastapi import FastAPI
 from core.config import settings
+from apis.general_pages.route_homepage import general_pages_router
 
-app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
 
-@app.get("/")
-def hello_api():
-    return {"msg":"Hello API"}
+def include_router(app):
+	app.include_router(general_pages_router)
+
+
+def start_application():
+	app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+	include_router(app)
+	return app
+
+
+app = start_application()
+
+
+# @app.get("/") #remove this, It is no longer needed.
+# def hello_api():
+#     return {"msg":"Hello API"}
+
+if __name__ == "__main__":
+	uvicorn.run('main:app')
